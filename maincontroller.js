3716e581bf054f725fe999d420d782638eb9ea8f
afope <fope.ojo@gmail.com>
2017-02-20 17:01:14 +0100

    no message

--- maincontroller.js
+++ maincontroller.js
@@ -6,7 +6,23 @@ console.log("Page Controller reporting for duty.");
 
 });
 
-app.controller('AboutCtrl', function ($scope, $location, $http) {
-console.log("About Controller reporting for duty.");
-
-});
+app.controller('githubDataController', function ($scope, $location, $http)
+{
+      $scope.getGitInfo = function () {
+         $scope.userNotFound = false;
+         $scope.loaded = false;
+         $http.get("https://api.github.com/users/" + $scope.username)
+               .success(function (data) {
+                  if (data.name == "") data.name = data.login;
+                  $scope.user = data;
+                  $scope.loaded = true;
+               })
+               .error(function () {
+                  $scope.userNotFound = true;
+               });
+         $http.get("https://api.github.com/users/" + $scope.username + "/repos").success(function (data) {
+            $scope.repos = data;
+            $scope.reposFound = data.length > 0;
+         });
+      }
+})
