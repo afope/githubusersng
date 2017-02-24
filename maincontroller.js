9710bcb51a501d7c18d76bdc16e153c2db50357d
afope <fope.ojo@gmail.com>
2017-02-24 17:40:03 +0100

    no message

--- maincontroller.js
+++ maincontroller.js
@@ -1,22 +1,24 @@
-app.controller('MainController', function($http) {
+app.controller('MainController',  ['$scope', '$http', function ($scope, $http) {
   var search = this;
   search.fetchData = function(user) {
     search.loading = true;
     search.data = false;
     $http.get('https://api.github.com/search/users?location=nigeria&q=' + user.name).then(function(response){
       console.log(response);
-      search.data = true;
-      search.name = response.data.name;
-      search.location = response.data.location;
-      search.bio = response.data.bio;
-      search.followers = response.data.followers;
-      search.public_repos = response.data.public_repos;
-      search.avatar = response.data.avatar_url;
-      search.loading = false;
-      search.isComplete = false;
+      search.data = true,
+      $scope.userinfo = [
+      search.name = response.data.name,
+      search.location = response.data.location,
+      search.bio = response.data.bio,
+      search.followers = response.data.followers,
+      search.public_repos = response.data.public_repos,
+      search.avatar = response.data.avatar_url,
+      search.loading = false,
+      search.isComplete = false
+    ]
     })
 }
-})
+}])
 app.controller('PageCtrl', function ($scope, $location, $http) {
 console.log("Page Controller reporting for duty.");
 
