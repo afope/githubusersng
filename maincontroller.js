af6aa87ef2e81fe9fd87da9818b149504925fc13
afope <fope.ojo@gmail.com>
2017-03-06 19:30:36 +0100

    no message

--- maincontroller.js
+++ maincontroller.js
@@ -18,23 +18,18 @@ app.controller('PageCtrl', function ($scope, $location, $http) {
 console.log("Page Controller reporting for duty.");
 });
 
-app.controller('UserCtrl', function ($scope, $http)
-{
-  var search = this;
-  search.getData = function(user) {
-  console.log(search.users);
-  $http.get('https://api.github.com/users/' + search.user.login).then(function(person){
-  search.data = true;
-  search.name = person.data.name;
-  search.location = person.data.location;
-  search.bio = person.data.bio;
-  search.followers = person.data.followers;
-  search.following = person.data.following;
-  search.repos = person.data.public_repos;
-  search.avatar = person.data.avatar_url;
-  console.log(search.name);
-  console.log(search.location);
-  console.log(search.bio);
-})
+app.controller('UserCtrl',  ['$scope', '$http', function ($scope, $http) {
+
+  var vm = this;
+  vm.data = false;
+  vm.fetchData = function (user) {
+    vm.loading = true;
+    $http.get('https://api.github.com/search/users/' + user.login).then(function(res)
+  {
+    console.log(res);
+    vm.data = true;
+    vm.name = res.data.name;
+    vm.locatin = res.data.location;
+  });
 }
-});
+}])
