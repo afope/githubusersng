8b89cc9329a3d2ba96b738c9c276f5e984b34da7
afope <fope.ojo@gmail.com>
2017-03-07 13:49:08 +0100

    no message

--- maincontroller.js
+++ maincontroller.js
@@ -5,29 +5,31 @@ app.controller('MainController',  ['$scope', '$http', function ($scope, $http) {
     $http.get('https://api.github.com/search/users?location=nigeria&q=' + user.name).then(function(response){
       //console.log(response);
       search.users = response.data.items;
-
-
-
-
     });
-
-
 }
 }])
+
 app.controller('PageCtrl', function ($scope, $location, $http) {
 console.log("Page Controller reporting for duty.");
 });
 
-app.controller('UserCtrl',  ['$scope', '$http', '$route', function ($scope, $http, $route) {
-
-  var user = $route.username;
+app.controller('UserCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+  console.log("User Controller reporting for duty.");
+  var person = this;
+  var user = $routeParams.username;
   console.log(user);
 
-    $http.get('https://api.github.com/search/users/' + user.login).then(function(res)
+    $http.get('https://api.github.com/users/' + user).then(function(res)
   {
     console.log(res);
     person.name = res.data.name;
+    console.log(person.name);
     person.location = res.data.location;
-  });
+    console.log(person.location);
+    person.bio = res.data.bio;
+    person.followers = res.data.followers;
+    person.following = res.data.following;
+    person.avatar =  res.data.avatar_url;
 
+  });
 }])
