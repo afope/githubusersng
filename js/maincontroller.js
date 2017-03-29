ecd36a4b7b8587ff6ce75e279ffbc127d19a5514
afope <fope.ojo@gmail.com>
2017-03-29 12:44:37 +0100

    no message

--- js/maincontroller.js
+++ js/maincontroller.js
@@ -1,6 +1,5 @@
 app.controller('MainController',  ['$scope', '$http', function ($scope, $http) {
   $scope.fetchData = function(user) {
-    $scope.data = false;
     $http.get('https://api.github.com/search/users?location=nigeria&q=' + user.name).then(function(response){
       $scope.users = response.data.items;
       console.log($scope.users);
